7cf4cc19f394579a7f62e220151ab0b04f95caf7
miriamfrank23 <miriamfrank23@gmail.com>
2018-08-10 12:28:35 -0400

    square_array

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |root|
+    new_array.push(root ** 2)
+  end
+  new_array
 end
+
+# def square_array(array)
+#   array.collect do |root|
+#     root ** 2
+#   end
+# end
